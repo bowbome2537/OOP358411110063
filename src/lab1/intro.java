d41c2e6087fa32139a155c88d292d5ddef66fcdd
bowbome2537 <chanyanut8446@gmail.com>
2018-11-19 16:34:32 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,24 @@
 package lab1;
 
 public class intro {
-}
+    public static void main(String[] args) {
+
+        System.out.println("chanyanut raksasaeng");
+        System.out.println("chanyanut8446@gmail.com");
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+}//class
