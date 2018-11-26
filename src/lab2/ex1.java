958ef2ef5c713447ccb02bdd43e3068d48bc6725
bowbome2537 <chanyanut8446@gmail.com>
2018-11-26 14:34:20 +0700

    Lab 2: Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,35 @@
+package lab2;
+
+import java.util.concurrent.SynchronousQueue;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        //integer
+        int a;
+        a=10;
+        System.out.println(a);
+        int b = 20;
+        System.out.println(b);
+        int c = a+b;
+        System.out.println("ผลรวมของ a และ b เท่ากับ "+c);
+
+        //real number
+        double w = 5.5;
+        System.out.println(w);
+        w = w * 10;
+        System.out.println(w);
+
+        //character
+        char d = '#';
+        System.out.println(d);
+
+        //string
+        String str = "chanyanut raksasaeng";
+        System.out.println("My name is " +str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+    }//main
+
+}//class
