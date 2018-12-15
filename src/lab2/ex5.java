f0f47002001d527b2e4cff5a16cdbf6a70464e40
bowbome2537 <chanyanut8446@gmail.com>
2018-12-15 15:02:17 +0700

    Lab 2: Variables

---
+++ src/lab2/ex5.java
@@ -0,0 +1,18 @@
+package lab2;
+
+public class ex5 {
+    public static void main(String[] args) {
+
+        for (int count =0; count <=100; count++){
+            if (count %3==0)
+            if (count %5==0)
+                System.out.println(count);
+
+        }
+
+    }//main
+
+
+
+}//class
+
